e00d4111fbecaf1263ab0e9e06db414ad180a861
tam03 <tamuramura2803@gmail.com>
2021-04-20 00:59:50 +0900

    rubocopの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for "User", at: "auth"
   root to: "top#index"
   get "top/index"
 end

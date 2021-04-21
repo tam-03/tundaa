024f01950a609e4717f0bc8a37045fb61c44f2a1
tam03 <tamuramura2803@gmail.com>
2021-04-22 02:05:47 +0900

    vue側からユーザーの登録とログインが出来る

--- app/javascript/login.vue
+++ app/javascript/login.vue
@@ -3,124 +3,42 @@
     <h2>ログイン</h2>
     <form>
       <v-text-field
-        v-model="name"
-        :error-messages="nameErrors"
-        :counter="10"
-        label="Name"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
+        v-model="email"
+        label="email"
       />
       <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
+        v-model="password"
+        label="password"
       />
-      <v-select
-        v-model="select"
-        :items="items"
-        :error-messages="selectErrors"
-        label="Item"
-        required
-        @change="$v.select.$touch()"
-        @blur="$v.select.$touch()"
-      />
-      <v-checkbox
-        v-model="checkbox"
-        :error-messages="checkboxErrors"
-        label="Do you agree?"
-        required
-        @change="$v.checkbox.$touch()"
-        @blur="$v.checkbox.$touch()"
-      />
-
       <v-btn
         class="mr-4"
-        @click="submit"
+        @click="login"
       >
-        submit
-      </v-btn>
-      <v-btn @click="clear">
-        clear
+        ログイン
       </v-btn>
     </form>
   </div>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email } from 'vuelidate/lib/validators'
+import axios from 'axios'
 
   export default {
-    mixins: [validationMixin],
-
-    validations: {
-      name: { required, maxLength: maxLength(10) },
-      email: { required, email },
-      select: { required },
-      checkbox: {
-        checked (val) {
-          return val
-        },
-      },
-    },
-
-    data: () => ({
-      name: '',
+  data () {
+    return {
       email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
-
-    computed: {
-      checkboxErrors () {
-        const errors = []
-        if (!this.$v.checkbox.$dirty) return errors
-        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-        return errors
-      },
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-      },
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
-    },
-
-    methods: {
-      submit () {
-        this.$v.$touch()
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-      },
-    },
+      passwaord:''
+    }
+  },
+  methods: {
+    login() {
+      axios.post('/api/auth/sign_in', {
+        email: this.email,
+        password: this.password,
+      }).then(response => {
+        console.log(response)
+      })
+    }
+  }
   }
 </script>
--- app/javascript/signup.vue
+++ app/javascript/signup.vue
@@ -1,5 +1,50 @@
 <template>
   <div>
-    <h2>アカウント登録</h2>
+    <h2>登録</h2>
+    <form>
+      <v-text-field
+        v-model="email"
+        label="email"
+      />
+      <v-text-field
+        v-model="password"
+        label="password"
+      />
+      <v-text-field
+        v-model="password_confirmation"
+        label="password_confirmation"
+      />
+      <v-btn
+        class="mr-4"
+        @click="register"
+      >
+        登録
+      </v-btn>
+    </form>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+  export default {
+  data () {
+    return {
+      email: '',
+      passwaord:'',
+      password_confirmation: ''
+    }
+  },
+  methods: {
+    register() {
+      axios.post('/api/auth', {
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.password_confirmation
+      }).then(response => {
+        console.log(response)
+      })
+    }
+  }
+  }
+</script>

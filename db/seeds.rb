1f3fd09cdb676d3e30ccc0e4f731d3d90104a89e
tam03 <tamuramura2803@gmail.com>
2021-04-26 10:36:42 +0900

    seedにデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  Template.create!(
+    title: "テンプレートタイトル#{n + 1}",
+    body: "テンプレートの内容 #{n + 1}"
+  )
+end
+
+3.times do |n|
+  Sample.create!(
+    title: "サンプルタイトル#{n + 1}",
+    body: "サンプルの内容 #{n + 1}"
+  )
+end

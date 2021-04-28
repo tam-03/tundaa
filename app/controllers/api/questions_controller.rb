5dceb51937b5b4f74f78e0bb4d9b8dbf3a87eaec
tam03 <tamuramura2803@gmail.com>
2021-04-29 00:01:14 +0900

    質問のAPIを作成

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,29 +1,43 @@
 # frozen_string_literal: true
 
 class API::QuestionsController < API::BaseController
+  before_action :authenticate_api_user!
+  before_action :set_question, only: %i[show update destroy]
+  protect_from_forgery except: %i[create update]
+
   def index
-    @question = Question.all
+    @questions = Question.all
   end
 
   def create
-    puts '------通過--------'
-    # current_userの情報が出るか確認
-    # puts current_user
-    # @question = Question.new(question_params)
-    # @question.user_id = current_user.id
+    @question = Question.new(question_params)
+    @question.user_id = current_api_user.id
+    if @question.save
+      head :created
+    else
+      head :bad_request
+    end
   end
 
-  def show
-    render json: Question.find(params[:id])
-  end
+  def show; end
 
   def update
+    if @question.update(question_params)
+      head :ok
+    else
+      head :bad_request
+    end
   end
 
   def destroy
+    @question.destroy
   end
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
---
+++ app/views/api/questions/_question.json.jbuilder
@@ -0,0 +1,4 @@
+json.id question.id
+json.title question.title
+json.body question.body
+json.user_id question.user_id

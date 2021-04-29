52abc98bdd8df3ca821cb510fe617287fc5d251a
tam03 <tamuramura2803@gmail.com>
2021-04-30 01:20:44 +0900

    ファイル構成を変更

--- app/javascript/NewQuestion.vue
+++ app/javascript/NewQuestion.vue
@@ -1,16 +1,72 @@
 <template>
   <div>
-    <h1>質問を作成する</h1>
-    <MarkdownEditor />
+    <h1>質問を作成</h1>
+    <v-container
+      fluid
+      class="grey lighten-5"
+    >
+      <input
+        v-model="title"
+        type="text"
+      >
+      <v-row
+        no-gutters
+      >
+        <v-col>
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            <v-textarea
+              v-model="content"
+              name="input-7-4"
+            />
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            <markdown-it-vue
+              class="md-body"
+              :content="content"
+            />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-btn @click="save">
+      保存する
+    </v-btn>
   </div>
 </template>
 
 <script>
-import MarkdownEditor from './MarkdownEditor'
+import MarkdownItVue from 'markdown-it-vue'
+import 'markdown-it-vue/dist/markdown-it-vue.css'
 
 export default {
   components: {
-    MarkdownEditor
+    MarkdownItVue
+  },
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    save() {
+      this.$store.dispatch('save', {
+        title: this.title,
+        content: this.content
+      })
+      this.title = ''
+      this.content = ''
+    }
   }
 }
 </script>

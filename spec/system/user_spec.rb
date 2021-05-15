00a74b69cd901ef4b65e3199363b28d0c6b734d7
tam03 <tamuramura2803@gmail.com>
2021-05-15 16:21:31 +0900

    アカウント登録とログインのテストを追加

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "User", type: :system do
+  scenario "登録", js: true do
+    visit "/sign_up"
+    expect(current_path).to eq "/sign_up"
+    fill_in "email", with: "alice@example.com"
+    fill_in "password", with: "testtest"
+    fill_in "password_confirmation", with: "testtest"
+    within "#register" do
+      click_button "登録"
+    end
+    expect(page).to have_content("アカウント登録が完了しました")
+  end
+  context "ログイン" do
+    let(:alice) { create(:user, email: "alice@example.com", password: "testtest") }
+    scenario "ログイン", js: true do
+      visit login_path
+      expect(current_path).to eq "/login"
+      fill_in "email", with: alice.email
+      fill_in "password", with: alice.password
+      within "#login" do
+        click_button "ログイン"
+      end
+      expect(page).to have_content("ログインしました")
+    end
+  end
+end

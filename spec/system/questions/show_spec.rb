5bfa151b9006142724efbfc8083796e4fadba4a0
tam03 <tamuramura2803@gmail.com>
2021-05-26 20:05:52 +0900

    質問の個別ページと一覧ページのテストを追加

---
+++ spec/system/questions/show_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Question show", type: :system do
+  context "認証なしのユーザーの場合" do
+    scenario "topにリダイレクトされる", js: true do
+      visit questions_path
+      expect(current_path).to eq "/login"
+    end
+  end
+
+  context "認証ありのユーザーの場合" do
+    let(:alice) { create(:user, email: "alice@example.com", password: "testtest") }
+    before do
+      create(:question, title: "Markdownが分からない", body: "## linkが分からない", user: alice)
+      visit login_path
+      fill_in "email", with: alice.email
+      fill_in "password", with: alice.password
+      within "#login" do
+        click_button "ログイン"
+      end
+      click_button "質問一覧"
+      click_link "Markdownが分からない"
+    end
+    scenario "コンテンツが表示されている", js: true do
+      expect(page).to have_content("Markdownが分からない")
+      expect(page).to have_content("linkが分からない")
+    end
+    scenario "クリックでコピーができる", js: true do
+      find("#markdown_preview").click
+      expect(page).to have_content("Markdownをコピーしました")
+    end
+    scenario "編集ページに遷移できる", js: true do
+      question = Question.find_by(title: "Markdownが分からない")
+      click_on "編集"
+      expect(current_path).to eq "/questions/#{question.id}/edit"
+      expect(page).to have_content("質問を編集")
+    end
+    scenario "質問の削除が出来る", js: true do
+      click_button "削除"
+      click_button "質問一覧"
+      expect(page).to_not have_content("Markdownが分からない")
+    end
+  end
+end

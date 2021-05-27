6708aae99db4755d1041824297cd3c7557e7703c
tam03 <tamuramura2803@gmail.com>
2021-05-27 12:04:40 +0900

    質問の編集ページを作り直した

--- app/javascript/EditQuestion.vue
+++ app/javascript/EditQuestion.vue
@@ -1,72 +1,160 @@
 <template>
-  <div>
-    <h1>質問を編集</h1>
-    <v-container
-      fluid
-      class="grey lighten-5"
+  <v-container
+    fluid
+  >
+    <div class="text-h2 text-center ma-5">
+      質問を編集
+    </div>
+    <div class="text-center mr-16 ml-16">
+      <v-text-field
+        id="title"
+        v-model="question.title"
+        label="タイトル"
+        color="orange lighten-1 accent-4"
+        :rules="[rules.required]"
+      />
+    </div>
+    <v-row
+      no-gutters
+      justify="center"
     >
-      <input
-        v-model="title"
-        type="text"
-      >
-      <v-row
-        no-gutters
-      >
-        <v-col>
-          <v-card
-            class="pa-2"
-            outlined
-            tile
+      <v-col id="input_area">
+        <label
+          for="body"
+          style="display: block; padding: 10px;"
+        >
+          内容
+        </label>
+        <v-textarea
+          id="input_markdown"
+          v-model="question.body"
+          name="body"
+          rows="11"
+          auto-grow
+          outlined
+          color="orange lighten-1 accent-4"
+        />
+      </v-col>
+      <v-col id="preview_and_sample_area">
+        <label
+          for="preview"
+        >
+          <v-btn
+            color="orange lighten-1 accent-4"
+            text
+            large
+            @click="preview = true"
           >
-            <v-textarea
-              v-model="content"
-              name="input-7-4"
-            />
-          </v-card>
-        </v-col>
-        <v-col>
+            プレビュー
+          </v-btn>
+          <v-btn
+            color="orange lighten-1 accent-4"
+            text
+            :disabled="!isAuthenticated"
+            large
+            @click="preview = false"
+          >
+            例文
+          </v-btn>
+        </label>
+        <template v-if="preview">
           <v-card
+            id="markdown_preview"
             class="pa-2"
             outlined
-            tile
+            min-height="275"
+            @click="doCopy"
           >
             <markdown-it-vue
               class="md-body"
-              :content="content"
+              :content="question.body"
             />
           </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-btn @click="editQuestion">
-      編集を保存
-    </v-btn>
-  </div>
+        </template>
+        <template v-else>
+          <v-card
+            class="pa-2"
+            outlined
+            min-height="275"
+          >
+            <SampleQuestion />
+          </v-card>
+        </template>
+      </v-col>
+    </v-row>
+    <div class="text-center ma-10">
+      <v-btn
+        id="save_question"
+        color="orange lighten-1 accent-4"
+        :disabled="!isAuthenticated || question.title == ''"
+        outlined
+        large
+        @click="editQuestion"
+      >
+        編集を保存する
+      </v-btn>
+      <div
+        v-if="!isAuthenticated"
+        class="text-caption"
+      >
+        会員登録をすると保存が出来ます
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import Vue from 'vue'
 import MarkdownItVue from 'markdown-it-vue'
 import 'markdown-it-vue/dist/markdown-it-vue.css'
+import SampleQuestion from './SampleQuestion.vue'
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
 
 export default {
   components: {
-    MarkdownItVue
+    MarkdownItVue,
+    SampleQuestion,
   },
   data() {
     return {
-      title: this.$store.getters.question.title,
-      content: this.$store.getters.question.body
+      preview: true,
+      rules: {
+        required: value => !!value || 'タイトルを入力して下さい',
+      },
     }
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.token !== null
+    },
+    question() {
+      return this.$store.getters.question
+    },
+  },
+  created() {
+    this.getQuestion()
   },
   methods: {
     editQuestion() {
       this.$store.dispatch('editQuestion', {
-        title: this.title,
-        body: this.content,
+        title: this.question.title,
+        body: this.question.body,
         id: this.$route.params.id
       })
-      this.title = ''
-      this.content = ''
+    },
+    getQuestion() {
+      this.$store.dispatch('getQuestion', {
+        questionId: this.$route.params.id
+      })
+    },
+    doCopy() {
+      this.$copyText(this.question.body).then(() => {
+        this.$store.dispatch('setAlert', {
+          type: "info",
+          message: "Markdownをコピーしました"
+        })
+      })
     }
   }
 }

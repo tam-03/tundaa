e20fb2c3b0fa05ce1db36fbfd15bffd123504ca9
tam03 <tamuramura2803@gmail.com>
2021-05-31 18:23:54 +0900

    サンプルの一覧を取得するテストを追加

---
+++ spec/system/samples/index_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Sample Index", type: :system do
+  context "認証ありのユーザーの場合" do
+    let(:alice) { create(:user, email: "alice@example.com", password: "testtest") }
+    let(:template) { create(:template, title: "何が分からないか分かっている", body: "## 困っていること") }
+    before do
+      create(:sample, title: "Markdownが分からない", body: "## linkが分からない", template_id: template.id)
+      create(:sample, title: "Linuxが分からない", body: "## パーミッションエラーが出る", template_id: template.id)
+      create(:sample, title: "Railsが分からない", body: "## Strong Parameterでエラーが出る", template_id: template.id)
+      visit login_path
+      fill_in "email", with: alice.email
+      fill_in "password", with: alice.password
+      within "#login" do
+        click_button "ログイン"
+      end
+      click_button "テンプレート"
+      click_on "何が分からないか分かっている"
+      click_link "例文一覧"
+    end
+    scenario "一覧が表示される", js: true do
+      expect(page).to have_content("Markdownが分からない")
+      expect(page).to have_content("Linuxが分からない")
+      expect(page).to have_content("Railsが分からない")
+    end
+  end
+end

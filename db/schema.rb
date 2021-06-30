5545c5f020f317c05b69087da8b608c59dfb7581
tam03 <tamuramura2803@gmail.com>
2021-06-30 11:30:57 +0900

    emailカラムにnull falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_005520) do
+ActiveRecord::Schema.define(version: 2021_06_30_022509) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_06_04_005520) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.json "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

28aea48b650a9e7c6abd78784a3e4b0ceee89c54
tam03 <tamuramura2803@gmail.com>
2021-07-21 16:59:48 +0900

    不要なコメントアウトの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token, if: :devise_controller?
-
-  # private
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  #   end
 end

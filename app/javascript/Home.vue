3fe83364e781d7b24420ee5b1ed23c24b86e8071
tam03 <tamuramura2803@gmail.com>
2021-09-03 19:59:44 +0900

    homeのデザインを修正

--- app/javascript/Home.vue
+++ app/javascript/Home.vue
@@ -1,70 +1,120 @@
 <template>
-  <v-container>
-    <div class="text-h2 text-center ma-10">
-      質問を作成する
+  <v-container class="d-flex justify-start align-center flex-column">
+    <div class="text-h3 text-center ma-10">
+      あなたの状態に近いものはどれ？
     </div>
-    <v-row
-      v-for="n in 1"
-      :key="n"
-      :class="n === 1 ? 'mb-6' : ''"
-      no-gutters
-    >
-      <v-col
-        v-for="template in templates"
-        :key="template.id"
-        class="ma-5"
-      >
+    <p class="text--secondary text-center mb-10">
+      分からないレベルを表しています。<br>今のあなたの状態に一番近いものを選択して質問文を作成してみよう！
+    </p>
+    <template v-if="overlay">
+      <v-overlay :value="overlay">
         <v-card
-          class="pa-2"
+          class="mx-auto"
           elevation="2"
-          shaped
-          :disabled="!isAuthenticated && !template.free ? disabled : !disabled"
-          :class="`template-${template.id}`"
+          outlined
         >
-          <v-icon
-            x-large
-            left
-          >
-            {{ icons[template.id - 1] }}
-          </v-icon>
-          <v-card-title>{{ template.title }}</v-card-title>
+          <v-card-title>{{ modalTemplate.title }}</v-card-title>
           <v-card-text>
-            <pre>{{ template.body }}</pre>
+            <pre>{{ modalTemplate.body }}</pre>
           </v-card-text>
           <v-card-actions>
-            <router-link
-              :to="`/questions/new/?template=${template.id}`"
-              class="text-decoration-none"
+            <v-btn
+              color="error"
+              @click="overlay = !overlay"
             >
-              <template v-if="!isAuthenticated && !template.free">
-                <v-btn
-                  color="orange lighten-1 accent-4"
-                  text
-                  large
-                >
-                  会員登録をするとご覧頂けます
-                </v-btn>
-              </template>
-              <template v-else>
-                <v-btn
-                  color="orange lighten-1 accent-4"
-                  text
-                  large
-                  @click="passTemplate(template)"
-                >
-                  作成
-                </v-btn>
-              </template>
-            </router-link>
+              閉じる
+            </v-btn>
           </v-card-actions>
         </v-card>
-      </v-col>
-    </v-row>
+      </v-overlay>
+    </template>
+    <div>
+      <v-row
+        v-for="template in templates"
+        :key="template.id"
+        class="template-border"
+      >
+        <v-col>
+          <v-card
+            :class="`template-${template.id} text-center d-flex justify-space-between align-center pa-5`"
+            :disabled="!isAuthenticated && !template.free"
+          >
+            <v-icon
+              x-large
+              center
+              class="mr-5"
+            >
+              {{ icons[template.id - 1] }}
+            </v-icon>
+            <div class="text-h5">
+              {{ template.title }}
+            </div>
+            <div class="d-flex flex-column">
+              <router-link
+                :to="`/questions/new/?template=${template.id}`"
+                class="text-decoration-none"
+              >
+                <template v-if="!isAuthenticated && !template.free">
+                  <v-btn
+                    color="grey darken-1"
+                    class="white--text pr-5 pl-5 mr-5 ml-5 mb-3"
+                  >
+                    要会員登録
+                  </v-btn>
+                </template>
+                <template v-else>
+                  <v-btn
+                    color="orange lighten-1 accent-2"
+                    class="white--text pr-5 pl-5 mr-5 ml-5 mb-3"
+                    @click="passTemplate(template)"
+                  >
+                    選択
+                  </v-btn>
+                </template>
+              </router-link>
+              <div>
+                <template v-if="!isAuthenticated && !template.free">
+                  <v-btn
+                    color="red darken-1"
+                    text
+                    small
+                  >
+                    会員登録が必要です
+                  </v-btn>
+                </template>
+                <template v-else>
+                  <v-btn
+                    color="orange lighten-1 accent-4"
+                    text
+                    small
+                    @click="openModal(template)"
+                  >
+                    内容を見る
+                  </v-btn>
+                </template>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <template v-if="!isAuthenticated">
+      <v-btn
+        color="orange lighten-1 accent-4"
+        x-large
+        class="mt-10 mb-5 white--text"
+        to="/sign_up"
+      >
+        会員登録
+      </v-btn>
+      <div class="text-body-1 text-center mb-5 text--secondary">
+        会員登録をすると全てのテンプレートを使用でき<br>質問文の保存や編集もできるようになります。
+      </div>
+    </template>
   </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   data() {
@@ -75,6 +125,7 @@ export default {
         "mdi-emoticon-confused-outline",
         "mdi-emoticon-dead-outline",
       ],
+      overlay: false,
     }
   },
   computed: {
@@ -83,6 +134,9 @@ export default {
     },
     templates() {
       return this.$store.getters.templates
+    },
+    modalTemplate() {
+      return this.$store.getters.template
     }
   },
   created() {
@@ -95,6 +149,13 @@ export default {
     passTemplate(template) {
       this.$store.dispatch('passTemplate', {
         template: template
+      })
+    },
+    openModal(template) {
+      this.$store.dispatch('passTemplate', {
+        template: template
+      }).then(() => {
+        this.overlay = true
       })
     }
   }
